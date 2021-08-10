8dd7272f29d6e45e4ef834f4aaca5d2c3c93c4a8
unknown <driyant.mail@gmail.com>
2021-08-10 13:33:15 +0700

    Assign data to components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import NoteList from "./components/NoteList.js";
 import "./App.css";
 
 class App extends Component {
+    state = [
+        {
+            id: 0,
+            title: "eat",
+            description: "reese peanut butter cups",
+            doesMatchSearch: true,
+        },
+        {
+            id: 1,
+            title: "sleep",
+            description: "eight hours",
+            doesMatchSearch: true,
+        },
+        {
+            id: 2,
+            title: "code",
+            description: "build an awesome ui",
+            doesMatchSearch: true,
+        },
+    ];
     render() {
         return (
             <div>
                 <Header />
-                <NoteList /> 
+                <NoteList />
             </div>
         );
     }

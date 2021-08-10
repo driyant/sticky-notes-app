2352253ef8ad8eba6b158119142c2fd453afb8ab
unknown <driyant.mail@gmail.com>
2021-08-10 13:25:02 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <h1 className="app-header__title">Super Sticky Notes</h1>
+                    <aside className="app-header__controls">
+                        <button className="add-new">+ New Note</button>
+                        <input
+                            type="text"
+                            placeholder="Type here to search..."
+                            className="search"
+                        />
+                    </aside>
+                </header>
+                <ul className="notes-list">
+                    <li className="note">
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            className="note__title"
+                        />
+                        <textarea
+                            placeholder="Description..."
+                            className="note__description"
+                        />
+                        <span className="note__delete">X</span>
+                    </li>
+                    <li className="note">
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            className="note__title"
+                        />
+                        <textarea
+                            placeholder="Description..."
+                            className="note__description"
+                        />
+                        <span className="note__delete">X</span>
+                    </li>
+                    <li className="note">
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            className="note__title"
+                        />
+                        <textarea
+                            placeholder="Description..."
+                            className="note__description"
+                        />
+                        <span className="note__delete">X</span>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
